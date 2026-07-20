7f660b256654f237f4f18cf06fbc6eaeb6ff26b0
noir@002 <paras.chauhan.93893@gmail.com>
2026-07-21 02:54:32 +0530

    add command

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 // yargs simplifies parsing and handling command-line arguments.
 
-const yargs = require("yargs/yargs");
+const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers"); // Passes only the user-provided arguments to yargs.
 
 const {initRepo} = require('./controllers/init');
+const {addRepo} = require('./controllers/add');
 
 yargs(hideBin(process.argv))
   .command("init", "Purpose: Initialize a new repository", {}, initRepo)
+  .command("add <file>", "Purpose: Add a file", (yargs) => {
+    yargs.positional("file", {
+        describe: "File to add to the staging area",
+        type: "String",
+    });
+  }, addRepo)
   .demandCommand(1, "Enter a command")
   .help()
   .parse();
